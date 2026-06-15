6108e2c18b36102aed13e51deb31d415cfbe3287
cristopher.salyrosas <cristopher.salyrosas@intellicare-peru.com>
2026-06-15 12:00:46 -0500

    Tensorflow&Pytorch_segmentationfix2

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import numpy as np
 import time
 import tempfile
 import os
+import torch
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1" # Previene Segmentation Fault por choque entre TF y PyTorch en la nube
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
